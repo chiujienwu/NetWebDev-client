1f0aa9b12dea90f8bce0f0489682a8d23ede065d
jgrissom <jgrissom91@gmail.com>
2019-01-19 16:29:49 -0600

    Modify the startup.cs file
    Enable static content

--- Startup.cs
+++ Startup.cs
@@ -25,10 +25,7 @@ namespace Client
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
         }
     }
 }
