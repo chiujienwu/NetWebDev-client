cba74d701371a80752500098c7cef89cd0745427
jgrissom <jgrissom91@gmail.com>
2019-01-27 15:27:40 -0600

    Grid - add a row with 3 columns

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,7 @@ namespace Client
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseDefaultFiles();
             app.UseStaticFiles();
         }
     }
