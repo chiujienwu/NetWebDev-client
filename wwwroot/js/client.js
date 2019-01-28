3d19ef30107652dae38ad5c4bcf9a5801f3c9095
jgrissom <jgrissom91@gmail.com>
2019-01-28 10:15:43 -0600

    Play sound effect on toast
    1) Create media folder inside wwwroot
    2) Copy toast.wav into media folder
    3) Modify js to preload and play sound

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -1,6 +1,11 @@
 ﻿$(function(){
+    // preload audio
+    var toast = new Audio('media/toast.wav');
+
     $('.code').on('click', function(e) {
         e.preventDefault();
+        // play audio
+        toast.play();
         $('#toast').toast({ autohide: false }).toast('show');
     });
 });
