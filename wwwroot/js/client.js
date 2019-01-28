b0017c07e994970ffda243268b85d5a8cde5f82a
jgrissom <jgrissom91@gmail.com>
2019-01-28 10:18:22 -0600

    Pause and reset the audio before playing

--- wwwroot/js/client.js
+++ wwwroot/js/client.js
@@ -4,6 +4,10 @@
 
     $('.code').on('click', function(e) {
         e.preventDefault();
+        // first pause the audio (in case it is still playing)
+        toast.pause();
+        // reset the audio
+        toast.currentTime = 0;
         // play audio
         toast.play();
         $('#toast').toast({ autohide: false }).toast('show');
