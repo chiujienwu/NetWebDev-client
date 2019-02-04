f75e7e5c8ae3234c7d75027c3584febb163e992b
jgrissom <jgrissom91@gmail.com>
2019-02-04 12:28:52 -0600

    Hide or show the balloons when the check boxes are checked or unchecked
    1) Attach an event listener to the checkbox change event using the “form-check-input” class selector
    2) Use the id of the checkbox to create a selector for the image

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -5,4 +5,13 @@
     $('.form-check-input').each(function () {
         $(this).prop('checked', false);
     });
+    // event listener for check/uncheck
+    $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+         $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+         $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+    });
 });
