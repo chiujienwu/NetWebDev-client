d0cef2d57203dc6f1a1d189ae9e8dc85d4ea3b0c
jgrissom <jgrissom91@gmail.com>
2019-02-04 10:44:59 -0600

    Uncheck all checkboxes & hide balloons by default when page loads

--- wwwroot/js/balloons.js
+++ wwwroot/js/balloons.js
@@ -1,3 +1,8 @@
 ﻿$(function(){
     $('#birthday').pickadate({ format: 'mmmm, d' });
+
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
 });
